6ccba6f189d2560950b70f45833e782c646b49c0
joal <joal@users.noreply.github.com>
2026-09-14 22:49:49 -0400

    init: empty repository

--- src/stm32_assert.h
+++
@@ -1,7 +0,0 @@
-#ifdef USE_FULL_ASSERT
-#include "main.h"
-
-#define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
-
-void assert_failed(uint8_t *file, uint32_t line);
-#endif
